2634e7416722602c05633a91efe7a81ed447e581
poohhsu <jerry0979323233@gmail.com>
2021-04-06 00:35:41 +0800

    Modify typesetting

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.photos').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    infinite: true,
+    focusOnSelect: true,
+    centerPadding: '8%',
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+});
